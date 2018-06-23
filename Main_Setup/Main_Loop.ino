d1bb97cdb88ff29fb328c1204a28cf0922af8728
Gabriel Green <gmpgreen16@gmail.com>
2018-06-22 21:12:09 -0700

    initial commit
    Includes Wifi basic testing

---
+++ Main_Setup/Main_Loop.ino
@@ -0,0 +1,32 @@
+void loop() {
+  delay(5000);
+  
+  pinMode(0, OUTPUT); //Setting GPIO 0 as output
+  digitalWrite(0, LOW); //Turning on LED (GPIO 0)
+  
+  WiFiClient client; // Use WiFiClient class to create TCP connections
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/testwifi/index.html";
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  
+  digitalWrite(0, HIGH); //Turning off LED (GPIO 0)
+  delay(500);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+}
---
+++ Main_Setup/Main_Setup.ino
@@ -0,0 +1,40 @@
+/*
+ *  Simple HTTP get webclient test
+ */
+ 
+#include <ESP8266WiFi.h>
+ 
+const char* ssid     = "TELUS4850";
+const char* password = "wnpfn2cc5h";
+ 
+const char* host = "wifitest.adafruit.com";
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+ 
+  // We start by connecting to a WiFi network
+ 
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password); //authenticating
+  
+  while (WiFi.status() != WL_CONNECTED) { //wating for connection
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+}
+ 
+
